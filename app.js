db9164ce9992083a8f1d83639a40204e09cd32ad
hellomac87 <hellomac87@gmail.com>
2019-03-18 23:06:10 +0900

    section 2

--- app.js
+++ app.js
@@ -35,3 +35,67 @@ car = {
     series: 3
 };
 console.log(car);
+// functions
+function returnMyName() {
+    return myName;
+}
+console.log(returnMyName());
+// void
+function sayHello() {
+    // this function should not anything
+    console.log("hello");
+    // return myName;
+}
+// argument types
+function multiply(value1, value2) {
+    return value1 * value2;
+}
+// console.log(multiply(1, 'Max'));
+console.log(multiply(2, 2));
+// function types
+var myMultiply;
+// myMultiply = sayHello;
+// myMultiply();
+myMultiply = multiply;
+console.log(myMultiply(5, 2));
+// objects
+var userData = {
+    name: "Max",
+    age: 27
+};
+userData = {
+    name: "asf",
+    age: 11
+};
+// complex object
+var complex = {
+    data: [100, 3.99, 10],
+    output: function (all) {
+        return this.data;
+    }
+};
+var complex2 = {
+    data: [100, 3.99, 10],
+    output: function (all) {
+        return this.data;
+    }
+};
+// union types
+var myRealRealAge = 27;
+myRealRealAge = "27";
+// myRealRealAge = true;
+// check types
+var finalValue = 30;
+if (typeof finalValue === "number") {
+    console.log("Final value is a number");
+}
+// never
+function neverReturns() {
+    throw new Error("An error!");
+}
+// Nullable Types
+var canBeNull = 12;
+canBeNull = null;
+var canAlsoBeNull;
+canAlsoBeNull = null;
+var canThisBeAny = null;
--- app.ts
+++ app.ts
@@ -42,6 +42,84 @@ car = {
 console.log(car);
 
 // functions
-function returnMyName() {
+function returnMyName(): string {
   return myName;
 }
+console.log(returnMyName());
+
+// void
+function sayHello(): void {
+  // this function should not anything
+  console.log("hello");
+  // return myName;
+}
+
+// argument types
+function multiply(value1: number, value2: number): number {
+  return value1 * value2;
+}
+// console.log(multiply(1, 'Max'));
+console.log(multiply(2, 2));
+
+// function types
+let myMultiply: (a: number, b: number) => number;
+// myMultiply = sayHello;
+// myMultiply();
+myMultiply = multiply;
+console.log(myMultiply(5, 2));
+
+// objects
+let userData: { name: string; age: number } = {
+  name: "Max",
+  age: 27
+};
+
+userData = {
+  name: "asf",
+  age: 11
+};
+
+// complex object
+let complex: { data: number[]; output: (all: boolean) => number[] } = {
+  data: [100, 3.99, 10],
+
+  output: function(all: boolean): number[] {
+    return this.data;
+  }
+};
+
+// complex = {};
+
+// type alias
+type Complex = { data: number[]; output: (all: boolean) => number[] };
+
+let complex2: Complex = {
+  data: [100, 3.99, 10],
+
+  output: function(all: boolean): number[] {
+    return this.data;
+  }
+};
+
+// union types
+let myRealRealAge: number | string = 27;
+myRealRealAge = "27";
+// myRealRealAge = true;
+
+// check types
+let finalValue = 30;
+if (typeof finalValue === "number") {
+  console.log("Final value is a number");
+}
+
+// never
+function neverReturns(): never {
+  throw new Error("An error!");
+}
+
+// Nullable Types
+let canBeNull: number | null = 12;
+canBeNull = null;
+let canAlsoBeNull;
+canAlsoBeNull = null;
+let canThisBeAny = null;
