981f4bf0a49dbefdbae081136465e6a5e803ca67
hellomac87 <hellomac87@gmail.com>
2019-03-20 23:46:59 +0900

    4rd

--- app.js
+++ app.js
@@ -1,78 +1,25 @@
 "use strict";
-// let & const
-console.log("%cLET & CONST", "color:red");
-var variable = "Test";
-console.log(variable);
-variable = "Another value";
-console.log(variable);
-var maxLevels = 100;
-console.log(maxLevels);
-// maxLevels = 99; // won`t work
-// Block scpoe
-function reset() {
-    // console.log(variable);
-    var variable = null;
-    console.log(variable);
-}
-reset();
-console.log(variable);
-// Arrow Functions
-console.log("%cARROW FUCNTIONS", "color:red");
-var addNumbers = function (number1, number2) {
-    return number1 + number2;
-};
-console.log(addNumbers(10, 3));
-var multiplyNumbers = function (number1, number2) {
-    return number1 * number2;
-};
-console.log(multiplyNumbers(2, 20));
-var greet = function () {
-    console.log("Hello");
+// Exercise 1
+var double = function (value) { return value * 2; };
+console.log(double(10));
+// Exercise 2
+var greet = function (name) {
+    if (name === void 0) { name = "Max"; }
+    console.log("Hello, " + name);
 };
 greet();
-var greetFriend = function (friend) {
-    console.log(friend);
-};
-greetFriend("Manu");
-// Default Parameters;
-console.log("%cDEFAULT PARAMETERS", "color:red");
-var countdown = function (start) {
-    if (start === void 0) { start = 10; }
-    console.log(start);
-    while (start > 0) {
-        start--;
-    }
-    console.log("Done!", start);
-};
-countdown(20);
-// Rest & Spread
-console.log("%cREST & SPREAD", "color:red");
-var numbers = [1, 10, 99, -5];
-console.log(Math.max(1, 10, 99, -5));
-console.log(Math.max.apply(Math, numbers));
-function makeArray(name) {
-    var args = [];
-    for (var _i = 1; _i < arguments.length; _i++) {
-        args[_i - 1] = arguments[_i];
-    }
-    return args;
-}
-console.log(makeArray("Max", 1, 22, 333));
-// Destructuring
-console.log("%cDESTRUCTURING(분해대입)", "color:red");
-var myHobbies = ["Cooking", "Sports"];
-// const hobby1 = myHobbies[0];
-// const hobby2 = myHobbies[1];
-var hobby1 = myHobbies[0], hobby2 = myHobbies[1];
-console.log(hobby1, hobby2);
-var userData = {
-    userName: "Max",
-    age: 27
-};
-var myName = userData.userName, myAge = userData.age;
-console.log(myName, myAge);
-// Template literals
-console.log("%cTEMPLATE LITERALS", "color:red");
-var userName = "Max";
-var greeting = "Hello, I'm " + userName + ",\nI'm so cool.";
-console.log(greeting);
+greet("Anna");
+// Exercise 3
+var numbers = [-3, 33, 38, 5];
+console.log(Math.min.apply(Math, numbers));
+// Exercise 4
+var newArray = [55, 20];
+newArray.push.apply(newArray, numbers);
+console.log(newArray);
+// Exercise 5
+var _a = [3.89, 2.99, 1.38], result1 = _a[0], result2 = _a[1], result3 = _a[2];
+console.log(result1, result2, result3);
+// Exercise 6
+var scientist = { firstName: "Will", experience: 12 };
+var firstName = scientist.firstName, experience = scientist.experience;
+console.log(firstName, experience);
--- app.ts
+++ app.ts
@@ -1,91 +1,28 @@
-// let & const
-console.log("%cLET & CONST", "color:red");
-let variable = "Test";
-console.log(variable);
-variable = "Another value";
-console.log(variable);
+// Exercise 1
+const double = (value: number): number => value * 2;
+console.log(double(10));
 
-const maxLevels = 100;
-console.log(maxLevels);
-// maxLevels = 99; // won`t work
-
-// Block scpoe
-function reset() {
-  // console.log(variable);
-  let variable = null;
-  console.log(variable);
-}
-reset();
-console.log(variable);
-
-// Arrow Functions
-console.log("%cARROW FUCNTIONS", "color:red");
-
-const addNumbers = function(number1: number, number2: number): number {
-  return number1 + number2;
+// Exercise 2
+const greet = (name: string = "Max"): void => {
+  console.log("Hello, " + name);
 };
-console.log(addNumbers(10, 3));
-
-const multiplyNumbers = (number1: number, number2: number): number => {
-  return number1 * number2;
-};
-
-console.log(multiplyNumbers(2, 20));
-
-const greet = (): void => {
-  console.log("Hello");
-};
-
 greet();
+greet("Anna");
 
-const greetFriend = (friend: string): void => {
-  console.log(friend);
-};
+// Exercise 3
+const numbers = [-3, 33, 38, 5];
+console.log(Math.min(...numbers));
 
-greetFriend("Manu");
+// Exercise 4
+const newArray = [55, 20];
+newArray.push(...numbers);
+console.log(newArray);
 
-// Default Parameters;
-console.log("%cDEFAULT PARAMETERS", "color:red");
+// Exercise 5
+const [result1, result2, result3] = [3.89, 2.99, 1.38];
+console.log(result1, result2, result3);
 
-const countdown = (start: number = 10): void => {
-  console.log(start);
-  while (start > 0) {
-    start--;
-  }
-  console.log("Done!", start);
-};
-
-countdown(20);
-
-// Rest & Spread
-console.log("%cREST & SPREAD", "color:red");
-const numbers = [1, 10, 99, -5];
-console.log(Math.max(1, 10, 99, -5));
-console.log(Math.max(...numbers));
-
-function makeArray(name: string, ...args: number[]): number[] {
-  return args;
-}
-console.log(makeArray("Max", 1, 22, 333));
-
-// Destructuring
-console.log("%cDESTRUCTURING(분해대입)", "color:red");
-const myHobbies = ["Cooking", "Sports"];
-// const hobby1 = myHobbies[0];
-// const hobby2 = myHobbies[1];
-const [hobby1, hobby2] = myHobbies;
-console.log(hobby1, hobby2);
-
-const userData = {
-  userName: "Max",
-  age: 27
-};
-const { userName: myName, age: myAge } = userData;
-console.log(myName, myAge);
-
-// Template literals
-console.log("%cTEMPLATE LITERALS", "color:red");
-const userName = "Max";
-const greeting = `Hello, I'm ${userName},
-I'm so cool.`;
-console.log(greeting);
+// Exercise 6
+const scientist = { firstName: "Will", experience: 12 };
+const { firstName, experience } = scientist;
+console.log(firstName, experience);
