6a069b36cbca4653d6455da2fd646135998bdb69
hellomac87 <hellomac87@gmail.com>
2019-03-24 15:38:04 +0900

    fucking modules end

--- app.ts
+++ app.ts
@@ -1,5 +1,6 @@
 import * as Circle from "./math/circle";
 import calculateRectangle from "./math/rectangle";
+
 console.log(Circle.PI);
 console.log(Circle.calculateCircumference(17));
 console.log(calculateRectangle(20, 123));
