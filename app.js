38818a84ad9a3822e41ef6bbfa3cc4e663a37027
hellomac87 <hellomac87@gmail.com>
2019-03-24 16:39:42 +0900

    this is interface, fuckers

--- app.js
+++ app.js
@@ -1,1 +1,46 @@
 "use strict";
+function greet(person) {
+    console.log("Hello, " + person.firstName);
+}
+function changeName(person) {
+    person.firstName = "Anna";
+}
+var person = {
+    firstName: "Max",
+    hobbies: ["Cooking", "Sports"],
+    greet: function (lastName) {
+        console.log("Hi, " + this.firstName + " " + lastName);
+    }
+};
+// greet({ firstName: "Max", age: 27 });
+changeName(person);
+greet(person);
+person.greet("Anything");
+var Person = /** @class */ (function () {
+    function Person() {
+        this.firstName = "";
+        this.lastName = "";
+    }
+    Person.prototype.greet = function (lastName) {
+        console.log("Hi, " + this.firstName + " " + lastName);
+    };
+    return Person;
+}());
+var myPerson = new Person();
+myPerson.firstName = "Maximillan";
+myPerson.lastName = "Anything";
+greet(myPerson);
+myPerson.greet("Anything");
+var myDoubleFunction;
+myDoubleFunction = function (value1, value2) {
+    return value1 + value2 * 2;
+};
+console.log(myDoubleFunction(10, 20));
+var oldPerson = {
+    age: 27,
+    firstName: "Max",
+    greet: function (lastName) {
+        console.log("Hello!");
+    }
+};
+console.log(oldPerson);
