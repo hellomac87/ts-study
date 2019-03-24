6330cf077cdb2183e5999b116a6a957c930c9d94
hellomac87 <hellomac87@gmail.com>
2019-03-24 14:39:21 +0900

    ㅗㅜ ㅑ

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
-"use strict";
+var MyMath;
+(function (MyMath) {
+    var Circle;
+    (function (Circle) {
+        var PI = 3.14;
+        function calculateCircumference(diameter) {
+            return diameter * PI;
+        }
+        Circle.calculateCircumference = calculateCircumference;
+    })(Circle = MyMath.Circle || (MyMath.Circle = {}));
+})(MyMath || (MyMath = {}));
+var MyMath;
+(function (MyMath) {
+    function calculateRectangle(width, height) {
+        return width * height;
+    }
+    MyMath.calculateRectangle = calculateRectangle;
+})(MyMath || (MyMath = {}));
+/// <reference path="circleMath.ts"/>
+/// <reference path="rectangleMath.ts"/>
+var CircleMath = MyMath.Circle;
+var PI = 2.99;
+console.log(MyMath.calculateRectangle(10, 20));
+console.log(CircleMath.calculateCircumference(3));
+console.log(PI);
