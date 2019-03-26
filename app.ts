cf92669915bc1c76fa07cd6bbb1c34961511ebe5
hellomac87 <hellomac87@gmail.com>
2019-03-27 00:02:10 +0900

    decorators

---
+++ app.ts
@@ -0,0 +1,33 @@
+function logged(constructorFn: Function) {
+  console.log(constructorFn);
+}
+
+@logged
+class Person {
+  constructor() {
+    console.log("Hi!");
+  }
+}
+
+// Factory
+function logging(value: boolean) {
+  return value ? logged : null;
+}
+
+@logging(true)
+class Car {}
+
+// Advanced
+function printable(constructorFn: Function) {
+  constructorFn.prototype.print = function() {
+    console.log(this);
+  };
+}
+
+@logging(false)
+@printable
+class Plant {
+  name = "Green Plant";
+}
+const plant = new Plant();
+(<any>plant).print();
