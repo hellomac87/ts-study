8eee150d9923bbf0f30dc896d437d3f5347b4aec
hellomac87 <hellomac87@gmail.com>
2019-03-27 22:35:33 +0900

    Decorators

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+var __param = (this && this.__param) || function (paramIndex, decorator) {
+    return function (target, key) { decorator(target, key, paramIndex); }
+};
 function logged(constructorFn) {
     console.log(constructorFn);
 }
@@ -47,3 +50,67 @@ var Plant = /** @class */ (function () {
 }());
 var plant = new Plant();
 plant.print();
+// Method Decorator
+// Property Decorator
+function editable(value) {
+    return function (target, propName, descriptor) {
+        descriptor.writable = value;
+    };
+}
+function overwritable(value) {
+    return function (target, propName) {
+        var newDescriptor = {
+            writable: value
+        };
+        return newDescriptor;
+    };
+}
+var Project = /** @class */ (function () {
+    function Project(name) {
+        this.projectName = name;
+    }
+    Project.prototype.calcBudget = function () {
+        console.log(1000);
+    };
+    __decorate([
+        overwritable(true)
+    ], Project.prototype, "projectName", void 0);
+    __decorate([
+        editable(true)
+    ], Project.prototype, "calcBudget", null);
+    return Project;
+}());
+var project = new Project("Super project");
+project.calcBudget();
+project.calcBudget = function () {
+    console.log(2000);
+};
+project.calcBudget();
+console.log(project.projectName);
+// Parameter Decorator
+function printInfo(target, methodName, paramIndex) {
+    console.log("Target: ", target);
+    console.log("MethodName: ", methodName);
+    console.log("ParamIndex: ", paramIndex);
+}
+var Course = /** @class */ (function () {
+    function Course(name) {
+        this.name = "";
+        this.name = name;
+    }
+    Course.prototype.printStudentNumbers = function (mode, printAll) {
+        if (printAll) {
+            console.log(10000);
+        }
+        else {
+            console.log(2000);
+        }
+    };
+    __decorate([
+        __param(1, printInfo)
+    ], Course.prototype, "printStudentNumbers", null);
+    return Course;
+}());
+var course = new Course("Super Course");
+course.printStudentNumbers("anything", true);
+course.printStudentNumbers("anything", false);
--- app.ts
+++ app.ts
@@ -31,3 +31,71 @@ class Plant {
 }
 const plant = new Plant();
 (<any>plant).print();
+
+// Method Decorator
+// Property Decorator
+function editable(value: boolean) {
+  return function(
+    target: any,
+    propName: string,
+    descriptor: PropertyDescriptor
+  ) {
+    descriptor.writable = value;
+  };
+}
+
+function overwritable(value: boolean) {
+  return function(target: any, propName: string): any {
+    const newDescriptor: PropertyDescriptor = {
+      writable: value
+    };
+    return newDescriptor;
+  };
+}
+class Project {
+  @overwritable(true)
+  projectName: string;
+  constructor(name: string) {
+    this.projectName = name;
+  }
+
+  @editable(true)
+  calcBudget() {
+    console.log(1000);
+  }
+}
+
+const project = new Project("Super project");
+
+project.calcBudget();
+project.calcBudget = function() {
+  console.log(2000);
+};
+project.calcBudget();
+console.log(project.projectName);
+
+// Parameter Decorator
+function printInfo(target: any, methodName: string, paramIndex: number) {
+  console.log("Target: ", target);
+  console.log("MethodName: ", methodName);
+  console.log("ParamIndex: ", paramIndex);
+}
+class Course {
+  name: string = "";
+
+  constructor(name: string) {
+    this.name = name;
+  }
+
+  printStudentNumbers(mode: string, @printInfo printAll: boolean) {
+    if (printAll) {
+      console.log(10000);
+    } else {
+      console.log(2000);
+    }
+  }
+}
+
+const course = new Course("Super Course");
+course.printStudentNumbers("anything", true);
+course.printStudentNumbers("anything", false);
